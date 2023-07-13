9779f368c51dd4b17c92957791c2ec6da5303540
g-sillva <silvagabriel.limars@gmail.com>
2023-07-13 18:47:15 -0300

    FLNow - Backend - Added jwt token in auth login

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     try {
@@ -25,8 +26,13 @@ export const login = async (req, res) => {
         const isPassCorrect = bcrypt.compareSync(req.body.password, user.password);
         if (!isPassCorrect) return res.status(400).send({ message: 'Wrong password.' });
 
+        const token = jwt.sign({
+            id: user._id,
+            isSeller: user.isSeller
+        }, process.env.JWT_KEY);
+
         const { ...info } = user;
-        res.status(200).send(info._doc);
+        res.cookie("accessToken", token, { httpOnly: true }).status(200).send(info._doc);
 
     } catch (err) {
         res.status(500).send({ message: err.message });
--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import userRoute from './routes/user.route.js';
 import serviceRoute from './routes/service.route.js';
@@ -22,6 +23,7 @@ try {
 }
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/users', userRoute);
 app.use('/api/services', serviceRoute);
