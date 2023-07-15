89b551e674eb5e36d2df5ec388aa4beb363ccba1
g-sillva <silvagabriel.limars@gmail.com>
2023-07-15 13:54:43 -0300

    FLNow - Frontend - Login functionality with axios

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 import userRoute from './routes/user.route.js';
 import serviceRoute from './routes/service.route.js';
@@ -23,6 +24,7 @@ try {
 }
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credential: true }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoute);
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { useState } from "react";
 import { GoogleLogin } from '@react-oauth/google';
 import "./Login.scss";
@@ -7,10 +8,28 @@ function Login() {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError("");
+
+      try {
+        const res = await axios.post("http://localhost:5432/api/auth/login", {
+          email,
+          password
+        });
+        console.log(res.data);
+      } catch (err) {
+        setError(err.response.data.message);
+        console.log(err);
+      }
+
+   }
 
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Login</h1>
 
         <div className="login-input-container">
@@ -33,6 +52,8 @@ function Login() {
           <i className={`fa-solid ${isPasswordVisible ? 'fa-eye-slash' : 'fa-eye'}`}
              onClick={() => setIsPasswordVisible(!isPasswordVisible)}></i>
         </div>
+
+        {error !== "" && <p className="login-error-message"><span>Error:</span> {error}</p>}
 
         <div className="login-bottom-row">
           <button type="submit">Login</button>
