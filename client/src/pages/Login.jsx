97b5f71c10620d08970e4f0846ccf838613830b2
g-sillva <silvagabriel.limars@gmail.com>
2023-07-14 21:20:40 -0300

    FLNow - Frontend - Added login css, reset css and enhanced login page html structure

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,24 +1,52 @@
 import React from "react";
+import { GoogleLogin } from '@react-oauth/google';
 import "./Login.scss";
 
 function Login() {
   return (
     <div className="login">
       <form>
-        <h1>Sign in</h1>
-        <label htmlFor="">Username</label>
-        <input
-          name="username"
-          type="text"
-          placeholder="johndoe"
-        />
+        <h1>Login</h1>
 
-        <label htmlFor="">Password</label>
-        <input
-          name="password"
-          type="password"
+        <div className="login-input-container">
+          <label htmlFor="">Email</label>
+          <input
+            name="email"
+            type="text"
+            placeholder="your@email.com"
+          />
+        </div>
+
+        <div className="login-input-container">
+          <label htmlFor="">Password</label>
+          <input
+            name="password"
+            type="password"
+          />
+        </div>
+
+        <div className="login-bottom-row">
+          <button type="submit">Login</button>
+          <p>Forgot password?</p>
+        </div>
+
+        <p className="login-bottom-account-text">Don't have an account? <span>create one now!</span></p>
+
+        <div className="login-form-topic-title">
+          <span></span>
+          <p>Or login with</p>
+          <span></span>
+        </div>
+
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
         />
-        <button type="submit">Login</button>
+        
       </form>
     </div>
   );
