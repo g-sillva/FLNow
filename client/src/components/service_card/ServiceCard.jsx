99e73eeba9eed117a9bd4e394f8d0b453ae9617b
g-sillva <silvagabriel.limars@gmail.com>
2023-08-26 22:05:11 -0300

    FLNOW - Frontend - Added service card css style

--- client/src/components/service_card/ServiceCard.jsx
+++ client/src/components/service_card/ServiceCard.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
+import "./ServiceCard.scss";
 
 function ServiceCard({ item }) {
   return (
@@ -7,22 +8,28 @@ function ServiceCard({ item }) {
       <div className="service_card">
         <img src={item.images[0]} alt="" />
         <div className="info">
-            <div className="user">
-              <img src={"/img/avatar.jpg"} alt="" />
-              <span>Username</span>
-            </div>
-          <p>{item.title}</p>
-          <div className="star">
-            <img src="./img/star.png" alt="" />
-            <span>
-              {!isNaN(item.totalStars / item.starNumber) &&
-                Math.round(item.totalStars / item.starNumber)}
-            </span>
+          <div className="user">
+            <img src={"/img/avatar.jpg"} alt="" />
+            <span>Username</span>
+          </div>
+          <div className="text">
+            <p className="title">{item.title}</p>
+            <p>
+              {item.shortDescription.length > 100
+                ? item.shortDescription.substring(0, 100) + "..."
+                : item.shortDescription}
+            </p>
           </div>
         </div>
         <hr />
         <div className="detail">
-          <img src="./img/heart.png" alt="" />
+          <div className="icons">
+            <img src="./img/heart.png" alt="" />
+            <div className="star">
+              <img src="./img/star.png" alt="" />
+              <span>10</span>
+            </div>
+          </div>
           <div className="price">
             <span>STARTING AT</span>
             <h2>$ {item.price}</h2>
@@ -30,7 +37,7 @@ function ServiceCard({ item }) {
         </div>
       </div>
     </Link>
-  )
+  );
 }
 
-export default ServiceCard
+export default ServiceCard;
