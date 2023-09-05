bb89340e4bfc8ead304e46acb60dc7e56f492b29
g-sillva <silvagabriel.limars@gmail.com>
2023-09-04 21:17:28 -0300

    FLNOW - Frontend - Added slider component to the service view page

--- client/src/pages/Service/Service.jsx
+++ client/src/pages/Service/Service.jsx
@@ -1,35 +1,36 @@
 import "./Service.scss";
 
-import React from 'react'
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
+import Slider from "infinite-react-carousel";
 import { Link, useParams } from "react-router-dom";
 
 function Service() {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const { isLoading, error, data } = useQuery({
-        queryKey: ["service"],
-        queryFn: () =>
-          newRequest.get(`/services/${id}`).then((res) => {
-            return res.data;
-          }),
-      });
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["service"],
+    queryFn: () =>
+      newRequest.get(`/services/${id}`).then((res) => {
+        return res.data;
+      }),
+  });
 
-      const userId = data?.userId;
+  const userId = data?.userId;
 
-      const {
-        isLoading: isLoadingUser,
-        error: errorUser,
-        data: dataUser,
-      } = useQuery({
-        queryKey: ["user"],
-        queryFn: () =>
-          newRequest.get(`/users/${userId}`).then((res) => {
-            return res.data;
-          }),
-        enabled: !!userId,
-      });
+  const {
+    isLoading: isLoadingUser,
+    error: errorUser,
+    data: dataUser,
+  } = useQuery({
+    queryKey: ["user"],
+    queryFn: () =>
+      newRequest.get(`/users/${userId}`).then((res) => {
+        return res.data;
+      }),
+    enabled: !!userId,
+  });
 
   return (
     <div className="service">
@@ -68,11 +69,16 @@ function Service() {
                 )}
               </div>
             )}
+            <Slider slidesToShow={1} arrowsScroll={1} className="slider">
+              {data.images.map((img) => (
+                <img key={img} src={img} alt="" />
+              ))}
+            </Slider>
           </div>
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default Service
+export default Service;
