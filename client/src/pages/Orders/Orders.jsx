46112c4075b4ce7a57a77f052fef3ef5918905f7
g-sillva <silvagabriel.limars@gmail.com>
2023-09-10 17:45:13 -0300

    FLNOW - Frontend - Added dinamic order data

--- client/src/pages/Orders/Orders.jsx
+++ client/src/pages/Orders/Orders.jsx
@@ -1,71 +1,65 @@
 import React from "react";
 import "./Orders.scss";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const Orders = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["orders"],
+    queryFn: () =>
+      newRequest.get('/orders').then((res) => res.data),
+  });
+
   const handleContact = () => {};
 
   return (
     <div className="orders">
-      <div className="container">
-        <h1 className="title">Orders</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Title</th>
-              <th>Price</th>
-              <th>Contact</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <img className="image" src="https://via.placeholder.com/150" alt="placeholder" />
-              </td>
-              <td>order title</td>
-              <td>order price</td>
-              <td>
-                <img
-                  src="./img/message.png"
-                  alt="placeholder"
-                  className="message"
-                  onClick={() => handleContact()}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img className="image" src="https://via.placeholder.com/150" alt="placeholder" />
-              </td>
-              <td>order title</td>
-              <td>order price</td>
-              <td>
-                <img
-                  src="./img/message.png"
-                  alt="placeholder"
-                  className="message"
-                  onClick={() => handleContact()}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img className="image" src="https://via.placeholder.com/150" alt="placeholder" />
-              </td>
-              <td>order title</td>
-              <td>order price</td>
-              <td>
-                <img
-                  src="./img/message.png"
-                  alt="placeholder"
-                  className="message"
-                  onClick={() => handleContact()}
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {isLoading ? (
+        "Loading"
+      ) : error ? (
+        "Something went wrong!"
+      ) : (
+        <div className="container">
+          <h1 className="title">Orders</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>{user.isSeller ? "Buyer" : "Seller"}</th>
+                <th>Contact</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((order) => (
+                <tr key={order._id}>
+                  <td>
+                    <img
+                      className="image"
+                      src={order.image}
+                      alt="placeholder"
+                    />
+                  </td>
+                  <td>{order.title}</td>
+                  <td>{order.price}</td>
+                  <td>{user.username}</td>
+                  <td>
+                    <img
+                      src="./img/message.png"
+                      alt="placeholder"
+                      className="message"
+                      onClick={() => handleContact()}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
