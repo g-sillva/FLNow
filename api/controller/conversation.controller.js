29d623d772bacc22d936e35276ccfe9e89257a44
g-sillva <silvagabriel.limars@gmail.com>
2023-09-13 16:15:18 -0300

    FLNOW - Backend - Added create conversation controller

--- api/controller/conversation.controller.js
+++ api/controller/conversation.controller.js
@@ -1,3 +1,5 @@
+import Conversation from '../models/conversation.model.js';
+
 export const getConversations = (req, res, next) => {
     try {
 
@@ -14,9 +16,18 @@ export const getConversation = (req, res, next) => {
     }
 }
 
-export const createConversation = (req, res, next) => {
-    try {
+export const createConversation = async (req, res, next) => {
+    const newConversation = new Conversation({
+        conversationId: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
+        sellerId: req.isSeller ? req.userId : req.body.to,
+        buyerId: req.isSeller ? req.body.to : req.userId,
+        readBySeller: req.isSeller,
+        readByBuyer: !req.isSeller,
+    });
 
+    try {
+        const savedConversation = await newConversation.save();
+        res.status(201).send(savedConversation);
     } catch(err) {
         next(err);
     }
