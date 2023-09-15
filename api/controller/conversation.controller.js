faf3d095c3115dc0522367851fd0736c1d6bae7e
g-sillva <silvagabriel.limars@gmail.com>
2023-09-14 22:35:54 -0300

    FLNOW - Backend - Added get conversations controller

--- api/controller/conversation.controller.js
+++ api/controller/conversation.controller.js
@@ -1,50 +1,61 @@
-import Conversation from '../models/conversation.model.js';
+import Conversation from "../models/conversation.model.js";
 
-export const getConversations = (req, res, next) => {
-    try {
-
-    } catch(err) {
-        next(err);
-    }
-}
+export const getConversations = async (req, res, next) => {
+  try {
+    const conversations = await Conversation.find(
+      req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }
+    );
+    res.status(200).send(conversations);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const getConversation = async (req, res, next) => {
-    try {
-        const conversation = await Conversation.findOne({ conversationId: req.params.id });
-        res.status(200).send(conversation);
-    } catch(err) {
-        next(err);
-    }
-}
+  try {
+    const conversation = await Conversation.findOne({
+      conversationId: req.params.id,
+    });
+    res.status(200).send(conversation);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const createConversation = async (req, res, next) => {
-    const newConversation = new Conversation({
-        conversationId: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
-        sellerId: req.isSeller ? req.userId : req.body.to,
-        buyerId: req.isSeller ? req.body.to : req.userId,
-        readBySeller: req.isSeller,
-        readByBuyer: !req.isSeller,
-    });
+  const newConversation = new Conversation({
+    conversationId: req.isSeller
+      ? req.userId + req.body.to
+      : req.body.to + req.userId,
+    sellerId: req.isSeller ? req.userId : req.body.to,
+    buyerId: req.isSeller ? req.body.to : req.userId,
+    readBySeller: req.isSeller,
+    readByBuyer: !req.isSeller,
+  });
 
-    try {
-        const savedConversation = await newConversation.save();
-        res.status(201).send(savedConversation);
-    } catch(err) {
-        next(err);
-    }
-}
+  try {
+    const savedConversation = await newConversation.save();
+    res.status(201).send(savedConversation);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const updateConversation = async (req, res, next) => {
-    try {
-        const updatedConversation = await Conversation.findOneAndUpdate({ conversationId: req.params.id}, {
-            $set: {
-                readBySeller: req.isSeller,
-                readByBuyer: !req.isSeller,
-            }
-        }, { new: true });
-        
-        res.status(200).send(updatedConversation);
-    } catch(err) {
-        next(err);
-    }
-}
+  try {
+    const updatedConversation = await Conversation.findOneAndUpdate(
+      { conversationId: req.params.id },
+      {
+        $set: {
+          readBySeller: req.isSeller,
+          readByBuyer: !req.isSeller,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).send(updatedConversation);
+  } catch (err) {
+    next(err);
+  }
+};
