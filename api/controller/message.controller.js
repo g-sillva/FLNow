2e0696cbf89b877af8b1e689afd54f235a7c3ebc
g-sillva <silvagabriel.limars@gmail.com>
2023-09-17 20:40:59 -0300

    FLNOW - Backend - Added create message route

--- api/controller/message.controller.js
+++ api/controller/message.controller.js
@@ -1,8 +1,23 @@
 import Message from "../models/message.model.js"
+import Conversation from "../models/conversation.model.js"
 
 export const createMessage = async (req, res, next) => {
+    const newMessage = new Message({
+        conversationId: req.body.conversationId,
+        userId: req.userId,
+        description: req.body.description
+    });
     try {
+        const savedMessage = await newMessage.save();
+        await Conversation.findOneAndUpdate({ conversationId: req.body.conversationId}, {
+            $set: {
+                readBySeller: req.isSeller,
+                readByBuyer: req.isBuyer,
+                lastMessage: req.body.description
+            }
+        }, { new: true });
 
+        res.status(201).json(savedMessage);
     } catch (err) {
         next(err);
     }
