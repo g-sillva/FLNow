55c64d4a76840c0b3ca3a9094b843e1561bfaa93
g-sillva <silvagabriel.limars@gmail.com>
2023-09-17 20:57:08 -0300

    FLNOW - Frontend - Added get dynamic messages in message page

--- client/src/pages/Message/Message.jsx
+++ client/src/pages/Message/Message.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Message.scss'
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
+import "./Message.scss";
 
 const Message = () => {
+  const { id } = useParams();
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["messages"],
+    queryFn: () => newRequest.get(`/messages/${id}`).then((res) => res.data),
+  });
+
   return (
     <div className="message">
-    <div className="container">
-      <span className="breadcrumbs">
-        <Link to="/messages">Messages</Link> {'>'} John Doe {'>'}
-      </span>
-        <div className="messages">
-            <div className="owner item">
-              <img
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-              <p>Description here</p>
-            </div>
-        </div>
-      <form className="write" >
-        <textarea type="text" placeholder="write a message" />
-        <button type="submit">Send</button>
-      </form>
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages">Messages</Link> {">"} John Doe {">"}
+        </span>
+        {isLoading ? (
+          "Loading..."
+        ) : error ? (
+          "Something went wrong!"
+        ) : (
+          <div className="messages">
+            {data.map((m, i) => (
+              <div className={m.userId === currentUser._id ? "owner item" : "item"} key={m._id} key={i}>
+                <img
+                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt=""
+                />
+                <p>{m.desc}</p>
+              </div>
+            ))}
+          </div>
+        )}
+        <form className="write">
+          <textarea type="text" placeholder="write a message" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
