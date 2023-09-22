f3cabc8939f0068ffac142a81dbcece01d229711
g-sillva <silvagabriel.limars@gmail.com>
2023-09-21 22:06:01 -0300

    FLNOW - Frontend: Added mocked data

---
+++ client/src/data.js
@@ -0,0 +1,103 @@
+export const cards = [
+    {
+      id: 1,
+      title: "Web Development",
+      desc: "Build your online presence",
+      img: "https://images.pexels.com/photos/207896/pexels-photo-207896.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      title: "Graphic Design",
+      desc: "Create stunning visuals",
+      img: "https://images.pexels.com/photos/10922/pexels-photo-10922.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 3,
+      title: "Content Writing",
+      desc: "Engage your audience with great content",
+      img: "https://www.betterteam.com/images/content-writer-job-description-4252x2845-20201118.jpeg?crop=40:21,smart&width=1200&dpr=2",
+    },
+    {
+      id: 4,
+      title: "Video Production",
+      desc: "Tell your story through video",
+      img: "https://images.pexels.com/photos/220037/pexels-photo-220037.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 5,
+      title: "Social Media Marketing",
+      desc: "Boost your online presence",
+      img: "https://images.pexels.com/photos/607812/pexels-photo-607812.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/3214953/pexels-photo-3214953.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Web Design",
+      username: "John Smith",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Logo Design",
+      username: "Emily Johnson",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/4144923/pexels-photo-4144923.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Video Editing",
+      username: "Michael Brown",
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/4348404/pexels-photo-4348404.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      cat: "Product Photography",
+      username: "Sarah Davis",
+    },
+  ];
+  
+  export const services = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/580151/pexels-photo-580151.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/720598/pexels-photo-720598.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "I will design a professional website for your business",
+      price: 199,
+      star: 4.5,
+      username: "Alice Miller",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/4145190/pexels-photo-4145190.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/1036627/pexels-photo-1036627.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "I will create eye-catching logos for your brand",
+      price: 99,
+      star: 5,
+      username: "David Wilson",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/8797307/pexels-photo-8797307.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/1062280/pexels-photo-1062280.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "I will write engaging content for your blog or website",
+      price: 49,
+      star: 4,
+      username: "Sophia Clark",
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/5708069/pexels-photo-5708069.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      pp: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "I will produce high-quality promotional videos for your business",
+      price: 249,
+      star: 4.5,
+      username: "James Anderson",
+    },
+  ];
+  
